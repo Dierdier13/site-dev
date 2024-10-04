cc4108064650a67f56ce6fc9876ab16582b984ec
Didier Piroille <didier.piroille@gmail.com>
2024-10-04 16:10:20 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('burger-icon').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active')
+});
